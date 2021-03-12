de5e6774b1e761a2aaae2041d51b4a410cf0fef0
neboer <rubinposter@gmail.com>
2021-03-12 23:22:55 +0800

    update upload class, need to write poll data.

--- src/net_process/net_upload.cpp
+++ src/net_process/net_upload.cpp
@@ -1,9 +1,49 @@
 #include "net_upload.h"
+#include <cstddef>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <future>
+// #include <curl/curl.h>
 
-void init_new_network_upload(string &url) {
+class full_upload_task
+{
+  CURLM *multi_handle;
+  string *target_url;
+  // notice here! if curl_mime has been created, the nbtp_chunk will be delete.
+  // the wrap_func is used for multiple picture bed upload
+  function<curl_mime *(nbtp_chunk *, CURL *)> wrap_func;
 
-}
+  full_upload_task(string target_url, function<curl_mime *(nbtp_chunk *, CURL *)> wrap_func)
+  {
+    multi_handle = curl_multi_init();
+    this->wrap_func = wrap_func;
+  }
 
-promise<string &> start_network_upload(nbtp_chunk *chunk) {
-    // 在main函数中，应该已经curl_global_init完毕了。
-}
+private:
+  static size_t server_response_collector(void *contents, size_t size, size_t nmemb, void *target_result_block_write_location)
+  {
+    size_t real_size = size * nmemb;
+    nbtp_chunk *target_chunk = (nbtp_chunk *)target_result_block_write_location;
+    target_chunk->insert(target_chunk->end(), (unsigned char *)contents, (unsigned char *)contents + real_size);
+    return real_size;
+  }
+
+public:
+  promise<string *> *add_task(nbtp_chunk *data_wait_for_upload)
+  {
+    CURL *current_easy_handle = curl_easy_init();
+    promise<string *> *promise_containing_result = new promise<string *>();
+    nbtp_chunk *server_response = new nbtp_chunk();
+    auto task_info = new communicate_message(promise_containing_result, server_response);
+    curl_easy_setopt(current_easy_handle, CURLOPT_MIMEPOST, wrap_func(data_wait_for_upload, current_easy_handle));
+    curl_easy_setopt(current_easy_handle, CURLOPT_URL, target_url->c_str());
+    curl_easy_setopt(current_easy_handle, CURLOPT_WRITEFUNCTION, server_response_collector);
+    curl_easy_setopt(current_easy_handle, CURLOPT_WRITEDATA, server_response);
+    curl_easy_setopt(current_easy_handle, CURLOPT_PRIVATE, task_info);
+    curl_multi_add_handle(multi_handle, current_easy_handle);
+    return promise_containing_result;
+  }
+
+  
+
+};
--- src/net_process/net_upload.h
+++ src/net_process/net_upload.h
@@ -1,6 +1,18 @@
 #include <future>
+#include <curl/curl.h>
 #include <string>
 #include "../nbtp.h"
 
+// 用来在子传输和主传输之间传输的结构体，如果传输完成，主传输将会获得这个结构体的内容。
+struct communicate_message
+{
+    communicate_message(promise<string *> *report_destination, nbtp_chunk *server_response){
+        this->report_destination = report_destination;
+        this->server_response = server_response;
+    }
+    promise<string *> *report_destination;
+    nbtp_chunk *server_response;
+};
+
 // 这里有一个什么问题：main函数需要先创建一个任务对象，这个任务对象将会影响下面这个函数的行为。首次调用创建任务，再次调用添加任务。这个函数应该对上传任务是透明的。
-promise<string &> start_network_upload(nbtp_chunk *chunk);// 这个函数有必要回收chunk。
+// promise<string &> start_network_upload(nbtp_chunk *chunk);// 这个函数有必要回收chunk。
