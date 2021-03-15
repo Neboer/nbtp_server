c8e8d5b6b3e1b435210a645293c1383141049945
Neboer <rubinposter@gmail.com>
2021-03-15 08:00:27 +0800

    phone update

--- src/net_process/net_upload.cpp
+++ src/net_process/net_upload.cpp
@@ -1,7 +1,43 @@
 #include "net_upload.h"
+#include <cstddef>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <future>
+// #include <curl/curl.h>
+
+class full_upload_task {
+  CURLM* multi_handle;
+  string* target_url;
+  // notice here! if curl_mime has been created, the nbtp_chunk will be delete.
+  // the wrap_func is used for multiple picture bed upload
+  function<curl_mime*(nbtp_chunk*, CURL*)> wrap_func;
+  
+  full_upload_task(string target_url, function<curl_mime*(nbtp_chunk*, CURL*)>wrap_func){
+    multi_handle = curl_multi_init();
+    this->wrap_func = wrap_func;
+  }
+  private:
+  static size_t server_response_collector(void *contents, size_t size, size_t nmemb, void *target_result_block_write_location){
+    size_t real_size = size* nmemb;
+    nbtp_chunk* target_chunk = (nbtp_chunk*) target_result_block_write_location;
+    target_chunk->insert(target_chunk->end(),(unsigned char*) contents, (unsigned char*) contents + real_size);
+    return real_size;
+  }
+  
+  public:
+  promise<string*>* add_task(nbtp_chunk* data_wait_for_upload){
+    CURL* current_easy_handle = curl_easy_init();
+    promise<string*>* promise_containing_result = new promise<string*>();
+    nbtp_chunk server_response
+    curl_easy_setopt(current_easy_handle, CURLOPT_MIMEPOST, wrap_func(data_wait_for_upload ,current_easy_handle));
+    curl_easy_setopt(current_easy_handle, CURLOPT_URL, target_url->c_str());
+    curl_easy_setopt(current_easy_handle, CURLOPT_WRITEFUNCTION, server_response_collector);
+  }
+  
+}
 
 void init_new_network_upload(string &url) {
-
+ 
 }
 
 promise<string &> start_network_upload(nbtp_chunk *chunk) {
