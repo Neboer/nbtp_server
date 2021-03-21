811c9eaef9ba29458e5d8572d012face3693aeee
neboer <rubinposter@gmail.com>
2021-03-22 07:39:17 +0800

    upload clang format file

--- src/net_process/net_upload.cpp
+++ src/net_process/net_upload.cpp
@@ -16,17 +16,18 @@ full_upload_task::full_upload_task(
   this->convert_to_uploadable_object = convert_to_uploadable_object;
 }
 
-size_t full_upload_task::server_response_collector (void *contents, size_t size, size_t nmemb,
-                            void *target_result_block_write_location) {
-    size_t real_size = size * nmemb;
-    nbtp_chunk *target_chunk = (nbtp_chunk *)target_result_block_write_location;
-    target_chunk->insert(target_chunk->end(), (unsigned char *)contents,
-                         (unsigned char *)contents + real_size);
-    return real_size;
+size_t full_upload_task::server_response_collector(
+    void *contents, size_t size, size_t nmemb,
+    void *target_result_block_write_location) {
+  size_t real_size = size * nmemb;
+  nbtp_chunk *target_chunk = (nbtp_chunk *)target_result_block_write_location;
+  target_chunk->insert(target_chunk->end(), (unsigned char *)contents,
+                       (unsigned char *)contents + real_size);
+  return real_size;
 }
 
 int full_upload_task::add_task(unique_ptr<nbtp_chunk> data_wait_for_upload) {
-  CURL* curl_obj = convert_to_uploadable_object(move(data_wait_for_upload));
+  CURL *curl_obj = convert_to_uploadable_object(move(data_wait_for_upload));
   curl_multi_add_handle(multi_handle, curl_obj);
   int running_handles_count = 0;
   curl_multi_perform(multi_handle, &running_handles_count);
--- src/upload.cpp
+++ src/upload.cpp
@@ -8,8 +8,7 @@
 #include <vector>
 
 using namespace std;
-void generator_manager(function<indexed_nbtp_chunk()> generator,
-                       ThreadsafeQueue<indexed_nbtp_chunk> *generate_queue) {
+void generator_manager(function<indexed_nbtp_chunk()> generator, ThreadsafeQueue<indexed_nbtp_chunk> *generate_queue) {
   indexed_nbtp_chunk last_generated_result;
   do {
     last_generated_result = generator();
@@ -20,24 +19,17 @@ void generator_manager(function<indexed_nbtp_chunk()> generator,
 // buffer2到key的过程，waiting_upload_queue是等待上传的、已经转换完成的png块的队列，在这里维护了一个upload_task_list，存储着需要等待的上传任务列表。
 void transformer_manager(
     function<int(indexed_nbtp_chunk)> start_upload_bk, // 非阻塞的启动上传
-    function<indexed_key_str()>
-        wait_for_an_result, // 阻塞等待之前安排的所有上传任务
-    ThreadsafeQueue<indexed_nbtp_chunk> *waiting_upload_queue,
-    function<void(indexed_key_str)> key_sender,
-    int concurrent_upload_proc_count) {
+    function<indexed_key_str()> wait_for_an_result,    // 阻塞等待之前安排的所有上传任务
+    ThreadsafeQueue<indexed_nbtp_chunk> *waiting_upload_queue, function<void(indexed_key_str)> key_sender, int concurrent_upload_proc_count) {
 
   indexed_nbtp_chunk current_upload_task = waiting_upload_queue->pop();
   int current_upload_count = 1;
-  while (current_upload_count >
-         0) { // 只要还有正在上传的任务，主循环就不能停下。
-    while (current_upload_count < concurrent_upload_proc_count &&
-           current_upload_task.index) {
-      current_upload_count = start_upload_bk(move(
-          current_upload_task)); // 在下面的阻塞等待中，一旦有一个上传完成，立刻准备拿出新的开始上传，同时也可以等之前的转换一段时间。
+  while (current_upload_count > 0) { // 只要还有正在上传的任务，主循环就不能停下。
+    while (current_upload_count < concurrent_upload_proc_count && current_upload_task.index) {
+      current_upload_count = start_upload_bk(move(current_upload_task)); // 在下面的阻塞等待中，一旦有一个上传完成，立刻准备拿出新的开始上传，同时也可以等之前的转换一段时间。
       current_upload_task = waiting_upload_queue->pop();
     }
-    indexed_key_str key =
-        wait_for_an_result(); // 等待一个上传结果，如果有则立即返回
+    indexed_key_str key = wait_for_an_result(); // 等待一个上传结果，如果有则立即返回
     key_sender(move(key));
   }
 }
@@ -55,5 +47,5 @@ void converter_manager(
         current_ori_data.index, move(current_converted_chunk));
     waiting_upload_queue->push(current_converted_data);
   }
-  waiting_upload_queue->push(indexed_nbtp_chunk());// 一旦到队列末尾，放入一个空的符号标记队列结束。
+  waiting_upload_queue->push(indexed_nbtp_chunk()); // 一旦到队列末尾，放入一个空的符号标记队列结束。
 }
