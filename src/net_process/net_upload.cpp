998597ac89aa32db174cc8cc7a67d7f2426f5f0c
neboer <rubinposter@gmail.com>
2021-03-23 13:42:28 +0800

    net upload

--- src/net_process/net_upload.cpp
+++ src/net_process/net_upload.cpp
@@ -10,15 +10,13 @@ using namespace std;
 
 full_upload_task::full_upload_task(
     string *target_url,
-    function<CURL *(unique_ptr<nbtp_chunk>)> convert_to_uploadable_object) {
+    function<void(CURL *, unique_ptr<nbtp_chunk>)> convert_to_uploadable_object) {
 
   this->multi_handle = curl_multi_init();
   this->convert_to_uploadable_object = convert_to_uploadable_object;
 }
 
-size_t full_upload_task::server_response_collector(
-    void *contents, size_t size, size_t nmemb,
-    void *target_result_block_write_location) {
+size_t full_upload_task::server_response_collector(void *contents, size_t size, size_t nmemb, void *target_result_block_write_location) {
   size_t real_size = size * nmemb;
   nbtp_chunk *target_chunk = (nbtp_chunk *)target_result_block_write_location;
   target_chunk->insert(target_chunk->end(), (unsigned char *)contents,
@@ -27,10 +25,12 @@ size_t full_upload_task::server_response_collector(
 }
 
 int full_upload_task::add_task(unique_ptr<nbtp_chunk> data_wait_for_upload) {
-  CURL *curl_obj = convert_to_uploadable_object(move(data_wait_for_upload));
-  
-  curl_easy_setopt(curl_obj, CURLOPT_PRIVATE, )
-  curl_multi_add_handle(multi_handle, curl_obj);
+  CURL *task_handle = curl_easy_init();
+  convert_to_uploadable_object(task_handle, move(data_wait_for_upload)); // 将需要上传的信息数据等绑定上
+  curl_easy_setopt(task_handle, CURLOPT_WRITEFUNCTION, server_response_collector);
+  curl_easy_setopt(task_handle, CURLOPT_WRITEDATA, )
+      curl_easy_setopt(curl_obj, CURLOPT_PRIVATE, )
+          curl_multi_add_handle(multi_handle, curl_obj);
   int running_handles_count = 0;
   curl_multi_perform(multi_handle, &running_handles_count);
   return running_handles_count;
@@ -46,16 +46,16 @@ indexed_key_str full_upload_task::wait_for_an_result() {
     curl_multi_wait(multi_handle, nullptr, 0, 0, &numfds);
     current_curl_message = curl_multi_info_read(multi_handle, &msgs);
   }
-  CURL* current_handle = current_curl_message->easy_handle;
-  if(current_curl_message->data.result == CURLE_OK){
+  CURL *current_handle = current_curl_message->easy_handle;
+  if (current_curl_message->data.result == CURLE_OK) {
     message current_message;
-    curl_easy_getinfo(current_handle, CURLINFO_PRIVATE, (void*)&current_message);
+    curl_easy_getinfo(current_handle, CURLINFO_PRIVATE, (void *)&current_message);
     return move(current_message.result);
   } else {
     message current_message;
-    curl_easy_getinfo(current_handle, CURLINFO_PRIVATE, (void*)&current_message);
-    if (current_message.failed_count > ) {
-      
-	}
+    curl_easy_getinfo(current_handle, CURLINFO_PRIVATE, (void *)&current_message);
+    if (current_message.failed_count > max_failed_count) {
+      throw max_retry_time_exceed{};
+    }
   }
 }
--- src/net_process/net_upload.h
+++ src/net_process/net_upload.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <curl/curl.h>
 #include <curl/multi.h>
+#include <exception>
 #include <future>
 #include <memory>
 #include <string>
@@ -11,10 +12,10 @@
 // 主上传任务和子上传任务之间传递的结构体就是indexed_nbtp_chunk。这个结构体在创建任务的时候被初始化，然后在从线程中取下来的时候返回。
 class full_upload_task {
   CURLM *multi_handle;
-  shared_ptr<string> target_url; // 这个string并没有被多线程访问，要限制这个访问仅限于此线程中。
-  function<CURL *(unique_ptr<nbtp_chunk>)> convert_to_uploadable_object; // 将一个二进制数据块变成curl_easy_handle，以便于加入multi传输过程。
-  int max_failed_count; // max try time after one failed upload.
-  full_upload_task(string *target_url, function<CURL *(unique_ptr<nbtp_chunk>)> convert_to_uploadable_object);
+  shared_ptr<string> target_url;                                               // 这个string并没有被多线程访问，要限制这个访问仅限于此线程中。
+  function<void(CURL *, unique_ptr<nbtp_chunk>)> convert_to_uploadable_object; // 传入一个普通的curl_easy_handle和一个二进制数据块，此方法应该将数据块作为文件内容附加在easy_handle中。
+  int max_failed_count;                                                        // max try time after one failed upload.
+  full_upload_task(string *target_url, function<void(CURL *, unique_ptr<nbtp_chunk>)> convert_to_uploadable_object);
 
 private:
   static size_t server_response_collector(void *contents, size_t size, size_t nmemb, void *target_result_block_write_location);
@@ -22,7 +23,7 @@ private:
 
 public:
   int add_task(unique_ptr<nbtp_chunk> data_wait_for_upload); // 添加一个上传任务，同时返回现在正在传输的上传任务的数量。
-  indexed_key_str wait_for_an_result();// 阻塞的等待一个上传任务结束，内部使用curl poll来实现。
+  indexed_key_str wait_for_an_result();                      // 阻塞的等待一个上传任务结束，内部使用curl poll来实现。
 };
 
 // data transferred between curl single handle and multi interface.
@@ -30,3 +31,5 @@ struct message {
   int failed_count = 0; // counter of failed transfer
   indexed_key_str result;
 };
+
+struct max_retry_time_exceed : exception {};
